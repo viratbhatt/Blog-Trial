d6b5e72f38e1b1374ff1414a9aad12be7a4886f1
virat bhatt <viratbhatt007@gmail.com>
2020-04-07 19:44:57 -0700

    New update

--- Trial/settings.py
+++ Trial/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'storages'
 ]
 
 MIDDLEWARE = [
@@ -140,5 +141,14 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'viratbhatt1960@gmail.com'
-EMAIL_HOST_PASSWORD = 'ehzdaycanjdjjlip'
+EMAIL_HOST_USER = os.environ.get('email_host_user')
+EMAIL_HOST_PASSWORD = os.environ.get('email_host_password')
+
+AWS_ACCESS_KEY_ID = os.environ.get('access-key-id')
+AWS_SECRET_ACCESS_KEY = os.environ.get('secret-key')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('storage-bucket-name')
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+
+DEFAULT_FILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
