093074b30f3e7ff5e873d44f1866d422aec902dd
virat bhatt <viratbhatt007@gmail.com>
2020-04-07 17:06:12 -0700

    requirements

--- Trial/settings.py
+++ Trial/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
 
 #IT WILL SAVE MEDIA FILES IN THE GIVEN LOCATION
