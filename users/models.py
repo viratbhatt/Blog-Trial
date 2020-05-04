9ed1d6a975ac449b68a16697785ab1a144731db3
virat bhatt <viratbhatt007@gmail.com>
2020-05-03 19:27:51 -0700

    New commit

--- users/models.py
+++ users/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from PIL import Image
 class Profile(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
-	image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+	image = models.ImageField(default='/media/default.jpg', upload_to='profile_pics')
 
 	def __str__(self):
 		return f'{self.user.username} Profile'
